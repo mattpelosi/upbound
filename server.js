e76813d3dc33977793d9ee8216e0d8ac95ba548c
Matthew Pelosi <mattpelosi@gmail.com>
2018-08-11 14:57:45 -0700

    initialize/basic server build

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+
+require("dotenv").config();
+app.use(bodyParser);
+
+app.use(express.static(path.join(`${__dirname}client/build`)));
+
+process.on("unhandledRejection", error => {
+  console.log("unhandled Rejection", error);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Server is listening on port ${port}`);
