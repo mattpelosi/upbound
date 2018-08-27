3d80867ad427a7ec5c8f9d6787dcd75e159aa13c
Matthew Pelosi <mattpelosi@gmail.com>
2018-08-26 18:50:12 -0700

    Progress bar state changes v1

--- client/src/components/Card/ProgressBar.js
+++ client/src/components/Card/ProgressBar.js
@@ -1,18 +1,66 @@
 import React from "react";
 
-function ProgressBar(props) {
-  return (
-    <div className="progress" style={progressStyle}>
-      <div
-        className="progress-bar"
-        role="progressbar"
-        aria-valuenow="50"
-        aria-valuemin="0"
-        aria-valuemax="100"
-        width={props.width}
-      />
-    </div>
-  );
+class ProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 0,
+      loading: false
+    };
+
+    this.startProgressFill = this.startProgressFill.bind(this);
+    this.stopProgressFill = this.stopProgressFill.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { currentWorkflow } = this.props;
+    const { loading } = this.state;
+
+    if (currentWorkflow === "pending") {
+      if (!loading) {
+        this.setState({ loading: true }, this.startProgressFill);
+      }
+    } else {
+      this.stopProgressFill();
+      if (loading) {
+        this.setState({ loading: false });
+      }
+    }
+  }
+
+  startProgressFill() {
+    let count = 0;
+    this.interval = setInterval(() => {
+      this.setState({ progress: count });
+      count = count + 10;
+    }, 100);
+  }
+
+  stopProgressFill() {
+    clearInterval(this.interval);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { progress, loading } = this.state;
+    return (
+      <div className="progress" style={progressStyle}>
+        <div
+          className="progress-bar"
+          role="progressbar"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          style={{
+            width: `${progress}%`,
+            background: `${loading ? "blue" : "green"}`
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 export default ProgressBar;
